888a1bd0a670ee95b4d8800806aba40209266374
Daniel Gális <danielgalis21@gmail.com>
2022-10-08 15:03:25 +0200

    get version name from tomado.py

--- setup.py
+++ setup.py
@@ -13,22 +13,22 @@
 ################################################################################
 
 from setuptools import setup
+from tomado import __version__
 
 APP = ['Tomado.py']
-VERSION = "0.2.3a"
 DATA_FILES = ["icons/tomado.icns", 'icons', 'sounds']
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
         'LSUIElement': True,
     },
-    'packages': ['rumps', 'pygame'],
+    'packages': ['rumps'],
     'iconfile': "icons/tomado.icns",
 }
 
 setup(
     app=APP,
-    version=VERSION,
+    version=__version__,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
