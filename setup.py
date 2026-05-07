62f5595bb6d820a44a363d48ae19ba484de31f29
Daniel Gális <danielgalis21@gmail.com>
2026-05-07 20:44:17 +0200

    migrate build tooling to uv

--- setup.py
+++ setup.py
@@ -13,17 +13,29 @@
 ################################################################################
 
 from setuptools import setup
+from py2app.build_app import py2app as _py2app
 from tomado import __version__
 
-APP = ['Tomado.py']
-DATA_FILES = ["icons/tomado.icns", 'icons', 'sounds']
+
+class py2app(_py2app):
+    # setuptools 61+ reads pyproject.toml and sets install_requires from
+    # [project].dependencies. py2app rejects any distribution that has it,
+    # expecting deps to already be installed (which uv handles). Clear it
+    # before the check runs.
+    def finalize_options(self):
+        self.distribution.install_requires = []
+        super().finalize_options()
+
+
+APP = ['tomado.py']
+DATA_FILES = ['icons', 'sounds']
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
         'LSUIElement': True,
     },
     'packages': ['rumps', 'pygame'],
-    'iconfile': "icons/tomado.icns",
+    'iconfile': 'icons/tomado.icns',
 }
 
 setup(
@@ -31,5 +43,5 @@ setup(
     version=__version__,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    cmdclass={'py2app': py2app},
 )
